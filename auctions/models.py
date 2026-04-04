de41f64102de52202ccdeb8690a87d76b8f3696b
João Duduch <jpduduch@gmail.com>
2026-04-04 17:51:11 +0200

    cleaning comments

--- auctions/models.py
+++ auctions/models.py
@@ -1,14 +1,14 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# handles users
+
 class User(AbstractUser):
     watchlist = models.ManyToManyField('Listing', blank=True)
 
     def __str__(self):
         return self.username
 
-# handles auction listings
+
 class Listing(models.Model):
     title = models.CharField(max_length=64)
     description = models.TextField()
